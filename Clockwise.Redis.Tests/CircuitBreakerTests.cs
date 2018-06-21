adee4486a0f1372d26f30eb0132cd44795be54f3
Diego <dicolomb@microsoft.com>
2018-06-21 23:03:49 +0100

    add circuit breaker
    add circuit breaking
    
    
    add test
    
    
    create redis storage and generic circuit breaker

---
+++ Clockwise.Redis.Tests/CircuitBreakerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using StackExchange.Redis;
+using Xunit;
+
+namespace Clockwise.Redis.Tests
+{
+    public class CircuitBreakerTests : IDisposable
+    {
+        [Fact]
+        public void SingalingState()
+        {
+            var cb01 = CircuitBreakerStorage.Create<string>("127.0.0.1");
+            cb01.StateDescriptor.State.Should().Be(CircuitBreakerState.Closed);
+            cb01.SetState(CircuitBreakerState.HalfClosed);
+            Task.Delay(100).Wait();
+            cb01.StateDescriptor.State.Should().Be(CircuitBreakerState.HalfClosed);
+        }
+
+        public void Dispose()
+        {
+          var connection =  ConnectionMultiplexer.Connect("127.0.0.1");
+            connection.GetDatabase().Execute("FLUSHALL");
+        }
+    }
+}
---
+++ Clockwise/CircuitBreaker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Clockwise
+{
+    public interface ICircuitBreaker
+    {
+        CircuitBreakerState State { get; }
+
+        CirtuitBreakerStateDescriptor StateDescriptor { get; }
+        void SetState(CircuitBreakerState newState, TimeSpan? expiry = null);
+    }
+
+    public interface ICircuitBreakerStorage : IDisposable
+    {
+        event EventHandler<CirtuitBreakerStateDescriptor> CircuitBreakerStateChanged;
+        CirtuitBreakerStateDescriptor StateDescriptor { get; }
+        void SetState(CircuitBreakerState newState, TimeSpan? expiry = null);
+    }
+
+    public sealed class CircuitBraker : ICircuitBreaker
+    {
+        private readonly ICircuitBreakerStorage _storage;
+
+        public CircuitBraker(ICircuitBreakerStorage storage)
+        {
+            _storage = storage ?? throw new ArgumentNullException(nameof(storage));
+            _storage.CircuitBreakerStateChanged += StorageOnCircuitBreakerStateChanged;
+        }
+
+        private void StorageOnCircuitBreakerStateChanged(object sender, CirtuitBreakerStateDescriptor e)
+        {
+            StateDescriptor = e;
+        }
+
+        public CircuitBreakerState State => StateDescriptor.State;
+        public CirtuitBreakerStateDescriptor StateDescriptor { get; private set; }
+        public void SetState(CircuitBreakerState newState, TimeSpan? expiry = null)
+        {
+            _storage.SetState(newState, expiry);
+        }
+    }
+}
