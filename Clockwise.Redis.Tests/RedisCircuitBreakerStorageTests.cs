b0ea0919b7ea179a5881d17289b1ec06aa2360c4
Diego <dicolomb@microsoft.com>
2018-06-26 10:42:41 +0100

    generalise circuitbreakerstorage tests

---
+++ Clockwise.Redis.Tests/RedisCircuitBreakerStorageTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Clockwise.Tests;
+using Pocket;
+using StackExchange.Redis;
+
+namespace Clockwise.Redis.Tests
+{
+    public class RedisCircuitBreakerStorageTests : CircuitBreakerStorageTests
+    {
+        protected override async Task<ICircuitBreakerStorage> CreateCircuitBreaker()
+        {
+            var cb01 = new CircuitBreakerStorage("127.0.0.1", 0, typeof(string));
+            await cb01.Initialise();
+            AddToDisposable(Disposable.Create(() =>
+            {
+                var connection = ConnectionMultiplexer.Connect("127.0.0.1");
+                connection.GetDatabase().Execute("FLUSHALL");
+                cb01.Dispose();
+                connection.Dispose();
+            }));
+            return cb01;
+        }
+
+        protected override IClock GetClock()
+        {
+            return Clock.Current;
+        }
+    }
+}
--- Clockwise.Tests/CircuitBreakerStorageTests.cs
+++ Clockwise.Tests/CircuitBreakerStorageTests.cs
@@ -1,36 +1,46 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentAssertions.Extensions;
-using StackExchange.Redis;
+using Pocket;
 using Xunit;
 
-namespace Clockwise.Redis.Tests
+namespace Clockwise.Tests
 {
-    public class CircuitBreakerStorageTests : IDisposable
+    public abstract class CircuitBreakerStorageTests : IDisposable
     {
+        private readonly CompositeDisposable disposables = new CompositeDisposable();
+        protected abstract Task<ICircuitBreakerStorage> CreateCircuitBreaker();
+
+        protected abstract IClock GetClock();
+        protected void AddToDisposable(IDisposable disposable)
+        {
+            disposables.Add(disposable);
+        }
+
         [Fact]
         public async Task When_signaling_failure_then_state_is_open()
         {
-            var cb01 = new CircuitBreakerStorage("127.0.0.1", 0,typeof(string));
-            await cb01.Initialise();
+            var cb01 = await CreateCircuitBreaker();
+            var clock = GetClock();
             var stateDescriptor = await cb01.GetLastStateAsync();
             stateDescriptor.Should().NotBeNull();
             await cb01.SignalFailureAsync(2.Seconds());
-            await Clock.Current.Wait(1.Seconds());
+            await clock.Wait(1.Seconds());
             stateDescriptor = await cb01.GetLastStateAsync();
             stateDescriptor.State.Should().Be(CircuitBreakerState.Open);
         }
 
+      
         [Fact]
         public async Task Give_an_open_circuitbreaker_then_signaling_succes_then_state_is_half_open()
         {
-            var cb01 = new CircuitBreakerStorage("127.0.0.1", 0, typeof(string));
-            await cb01.Initialise();
+            var cb01 = await CreateCircuitBreaker();
+            var clock = GetClock();
             await cb01.SignalFailureAsync(2.Seconds());
-            await Clock.Current.Wait(1.Seconds());
+            await clock.Wait(1.Seconds());
             await cb01.SignalSuccessAsync();
-            await Clock.Current.Wait(1.Seconds());
+            await clock.Wait(1.Seconds());
             var stateDescriptor = await cb01.GetLastStateAsync();
             stateDescriptor.State.Should().Be(CircuitBreakerState.HalfOpen);
         }
@@ -38,9 +48,9 @@ namespace Clockwise.Redis.Tests
         [Fact]
         public async Task Give_an_half_open_circuitbreaker_then_signaling_succes_then_state_is_closed()
         {
-            var cb01 = new CircuitBreakerStorage("127.0.0.1", 0, typeof(string));
-            await cb01.Initialise();
-           await cb01.SignalFailureAsync(TimeSpan.FromSeconds(2));
+            var cb01 = await CreateCircuitBreaker();
+            var clock = GetClock();
+            await cb01.SignalFailureAsync(TimeSpan.FromSeconds(2));
             await Clock.Current.Wait(1.Seconds());
             await cb01.SignalSuccessAsync();
             await Clock.Current.Wait(1.Seconds());
@@ -54,16 +64,16 @@ namespace Clockwise.Redis.Tests
         [Fact]
         public async Task Give_an_half_open_circuitbreaker_then_signaling_failure_then_state_is_closed()
         {
-            var cb01 = new CircuitBreakerStorage("127.0.0.1", 0, typeof(string));
-            await cb01.Initialise();
+            var cb01 = await CreateCircuitBreaker();
+            var clock = GetClock();
             await cb01.SignalFailureAsync(2.Seconds());
-            await Clock.Current.Wait(1.Seconds());
+            await clock.Wait(1.Seconds());
             await cb01.SignalSuccessAsync();
-            await Clock.Current.Wait(1.Seconds());
+            await clock.Wait(1.Seconds());
             var stateDescriptor = await cb01.GetLastStateAsync();
             stateDescriptor.State.Should().Be(CircuitBreakerState.HalfOpen);
             await cb01.SignalFailureAsync(2.Seconds());
-            await Clock.Current.Wait(1.Seconds());
+            await clock.Wait(1.Seconds());
             stateDescriptor = await cb01.GetLastStateAsync();
             stateDescriptor.State.Should().Be(CircuitBreakerState.Open);
         }
@@ -71,18 +81,17 @@ namespace Clockwise.Redis.Tests
         [Fact]
         public async Task When_open_state_expires_it_is_set_to_half_open()
         {
-            var cb01 = new CircuitBreakerStorage("127.0.0.1", 0, typeof(string));
-            await cb01.Initialise();
+            var cb01 = await CreateCircuitBreaker();
+            var clock = GetClock();
             await cb01.SignalFailureAsync(1.Seconds());
-            await Clock.Current.Wait(2.Seconds());
+            await clock.Wait(2.Seconds());
             var stateDescriptor = await cb01.GetLastStateAsync();
             stateDescriptor.State.Should().Be(CircuitBreakerState.HalfOpen);
         }
 
         public void Dispose()
         {
-          var connection =  ConnectionMultiplexer.Connect("127.0.0.1");
-            connection.GetDatabase().Execute("FLUSHALL");
+            disposables.Dispose();
         }
     }
 }
