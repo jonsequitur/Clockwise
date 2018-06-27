c74816e4e454811e211c19c277c482fa5286f3af
Diego <dicolomb@microsoft.com>
2018-06-27 17:47:19 +0100

    PassThroughPolicy<TChannel>

--- Clockwise/PassThroughPolicy.cs
+++ Clockwise/PassThroughPolicy.cs
@@ -7,8 +7,15 @@ namespace Clockwise
     {
         public override Task<ICommandDeliveryResult> Handle(HandleCommand<TChannel> handlerDelegate, ICommandDelivery<TChannel> delivery)
         {
-            if (handlerDelegate == null) throw new ArgumentNullException(nameof(handlerDelegate));
-            if (delivery == null) throw new ArgumentNullException(nameof(delivery));
+            if (handlerDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(handlerDelegate));
+            }
+
+            if (delivery == null)
+            {
+                throw new ArgumentNullException(nameof(delivery));
+            }
             return handlerDelegate(delivery);
         }
     }
